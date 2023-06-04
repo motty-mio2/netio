3d3b0339c146f865761af5c4154cb60d52d156b3
motty <motty.mio2@gmail.com>
2023-06-04 20:40:14 +0900

    update inadyn

--- netio/model/formats/inadyn.py
+++ netio/model/formats/inadyn.py
@@ -1,14 +1,16 @@
-from netio.model.base_host import configs, host
+from pathlib import Path
+from netio.model.base_host import parsed_data
+from netio.config import c
 
 
-def inadyn(configs: configs, filename: str = "inadyn.conf") -> None:
-    with open(filename, mode="w") as f:
+def inadyn(parsed_data: parsed_data, directory: Path = c.root_dir, filename: str = "inadyn.conf") -> None:
+    with open(directory / filename, mode="w") as f:
         f.write("provider cloudflare.com {\n")
-        f.write(f"    {configs.inadyn}\n")
+        f.write(f"    {parsed_data.inadyn}\n")
         f.write("    hostname = { \n")
-        for host in configs.hosts:
-            if host.external:
-                f.write(f"    {host.rule_name}.{configs.doamin},\n")
+        for h in parsed_data.hosts:
+            if h.external:
+                f.write(f"    {h.rule_name}.{parsed_data.doamin},\n")
 
         f.write("    }\n")
         f.write("}")
