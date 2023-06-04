c8b6119edcc366e8972a5403fbfd505364a805a9
motty <motty.mio2@gmail.com>
2023-06-04 22:00:22 +0900

    rename output file name

--- netio/model/formats/coredns.py
+++ netio/model/formats/coredns.py
@@ -7,6 +7,6 @@ ip = socket.gethostbyname(socket.gethostname())
 
 
 def coredns(parsed_data: parsed_data, filename: str = "mio2") -> None:
-    with open(c.export_dir / filename, mode="w") as f:
+    with open(c.export_dir / f"core.{filename}", mode="w") as f:
         for h in parsed_data.hosts:
             f.write(f"{ip} {h.rule_name}.{parsed_data.doamin}\n")
