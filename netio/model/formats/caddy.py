8dbf4c53a5d0f04cb508cfddcea35c75fa4c9ea2
motty <motty.mio2@gmail.com>
2023-06-04 20:39:26 +0900

    add caddy setting

--- netio/model/formats/caddy.py
+++ netio/model/formats/caddy.py
@@ -1,7 +1,21 @@
-from netio.model.base_host import configs, host
+from pathlib import Path
+from netio.model.base_host import parsed_data
+from netio.config import c
 
 
-def caddy(configs: configs, filename: str = "caddy") -> None:
-    with open(f"{host.rule_name}.{filename}", mode="w") as f:
-        for host in configs.hosts:
-            f.write(f"{host.ip_address} {host.rule_name}.{configs.doamin}\n")
+def caddy(parsed_data: parsed_data, directory: Path = c.root_dir, filename: str = "caddy") -> None:
+    for h in parsed_data.hosts:
+        dir: Path = Path()
+
+        if h.external:
+            dir = directory / "external"
+            dir.mkdir(exist_ok=True)
+        else:
+            dir = directory / "internal"
+            dir.mkdir(exist_ok=True)
+
+        with open(dir / f"{h.rule_name}.{filename}", mode="w") as f:
+            f.write((f"@{h.rule_name} host {h.rule_name}.{parsed_data.doamin}\n" f"handle @{h.rule_name} {'{'}\n"))
+            if h.service_domain:
+                f"\treverse_proxy {'{'} to {h.service_domain}:{h.port} {'}'}\n"
+            f.write("}")
